6605b90821177c94783ce9332dbdfb0c3680561d
Muhammad Faiq Al Harits <88996846+faiqalharits@users.noreply.github.com>
2023-03-07 09:27:27 +0700

    initial-commit

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,4 +1,134 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+
+<main id="gallery-page">
+
+    <div class="judul">
+        <h1>Galeri</h1>
+    </div>
+
+    <div class="container">
+        <div class="content">
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1007?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1007?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">
+
+                    </a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">Close</a>
+                </div>
+            </div>
+            <div class="item-galeri">
+                <a href="#popup-container">
+                    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                </a>
+                <div id="popup-container" class="popup-container">
+                    <div class="popup-image">
+                        <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+                    </div>
+                    <a href="#" class="popup-close">makan</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <a href="#popup-container">
+    <img src="https:/source.unsplash.com/1000x1005?school" alt="Image">
+  </a> -->
+</main>
 @endsection
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,14 +3,92 @@
 @section('content')
 
 <main id="news-page">
+    <div class="text-judul container">
+        <h1>News</h1>
+        <h3>SMAIT Baitul Ilmi</h3>
+    </div>
 
-    <h1>Semua Berita SMAIT Baitul 'Ilmi</h1>
+    <div class="image-grid container">
+        <div class="item-grid">
+            <div class="title-berita">
+                <h5>makan</h5>
+            </div>
+        </div>
+        <div class="item-grid">
+            <div class="title-berita">
+                <h5>makan</h5>
+            </div>
+        </div>
+        <div class="item-grid">
+            <div class="title-berita">
+                <h5>makan</h5>
+            </div>
+        </div>
+        <div class="item-grid">
+            <div class="title-berita">
+                <h5>makan</h5>
+            </div>
+        </div>
+    </div>
+    <div class="content container">
+        <div class="item-galeri">
+        </div>
+        <div class="item-galeri">
+        </div>
+        <div class="item-galeri">
+        </div>
+        <div class="item-galeri">
+        </div>
+        <div class="item-galeri">
+        </div>
+        <div class="item-galeri">
+        </div>
+        <div class="item-galeri">
+        </div>
+        <div class="item-galeri">
+        </div>
+    </div>
+
+
+    <!-- <div class="judul">
+        <h1>Semua Berita SMAIT Baitul 'Ilmi</h1>
+    </div>
 
     <section class="container">
+        <a href="#">
+            <article class="news-item">
+                <img src="https:/source.unsplash.com/800x800?school" alt="">
+                <h2>Baitul Ilmi</h2>
+                <p>20-10-2023</p>
+            </article>
+        </a>
+        <article class="news-item">
+            <img src="https:/source.unsplash.com/800x800?school" alt="">
+            <h2>Baitul Ilmi</h2>
+            <p>20-10-2023</p>
+        </article>
+        <article class="news-item">
+            <img src="https:/source.unsplash.com/800x800?school" alt="">
+            <h2>Baitul Ilmi</h2>
+            <p>20-10-2023</p>
+        </article>
+        <article class="news-item">
+            <img src="https:/source.unsplash.com/800x800?school" alt="">
+            <h2>Baitul Ilmi</h2>
+            <p>20-10-2023</p>
+        </article>
+        <article class="news-item">
 
-    
-
-    </section>
+            <img src="https:/source.unsplash.com/800x800?school" alt="">
+            <h2>Baitul Ilmi</h2>
+            <p>20-10-2023</p>
+        </article>
+        <article class="news-item">
+            <img src="https:/source.unsplash.com/800x800?school" alt="">
+            <h2>Baitul Ilmi</h2>
+            <p>20-10-2023</p>
+        </article>
+    </section> -->
 
 </main>
 
