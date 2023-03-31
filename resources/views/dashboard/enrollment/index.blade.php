1d9611486ce1f8b19eac14f86dc2d01c631bdf40
Muhammad Faiq Al Harits <88996846+faiqalharits@users.noreply.github.com>
2023-03-31 10:42:45 +0700

    paoq

--- resources/views/dashboard/enrollment/index.blade.php
+++ resources/views/dashboard/enrollment/index.blade.php
@@ -8,7 +8,18 @@
 </div>
 
 <div class="row py-4">
-
+    <div class="card text-center mb-3 d-flex justify-content-center align-items-center"
+        style="width: 18rem; height:4rem;">
+        <div class="d-grid gap-2 d-md-block">
+            <button class="btn btn-primary" type="button">Button</button>
+            <p>Kuota sudah penuh</p>
+        </div>
+    </div>
 </div>
 
+@endsection
+@section('css')
+<style>
+
+</style>
 @endsection
