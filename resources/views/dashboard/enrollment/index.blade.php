d1cc4f395f435759bed87740fd2348afba60bb57
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2023-05-03 08:42:48 +0700

    best

--- resources/views/dashboard/enrollment/index.blade.php
+++ resources/views/dashboard/enrollment/index.blade.php
@@ -8,6 +8,26 @@
 
 <div class="row">
     <div class="col-lg-6 pe-lg-3 mb-4">
+        <div class="card shadow rounded mb-4 p-4">
+            <div class="row">
+                <div class="col">
+                    <div>
+                        <p>
+                            Status Pendaftaran :
+                        </p>
+                        <h2 class="pt-2">Dibuka</h2>
+                    </div>
+                </div>
+                <div class="ms-auto col-2">
+                    <div class="btn btn-secondary d-flex align-items-center gap-2 py-2 px-3">
+                        <i class="bx bx-edit"></i>
+                        <p>
+                            Ubah
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="card shadow rounded">
             <div class="fs-4 justify-content-center d-flex mt-4">
                 <h3 class="fs-4 justify-content-center d-flex mt-3">Biaya Pendaftaran</h3>
@@ -49,7 +69,45 @@
             <button type="button" class="btn btn-primary p-2 m-4">Ubah Data</button>
         </div>
     </div>
-    <div class="col-lg-6 ps-lg-3">
+    <div class="col-lg-6 ps-lg-3 mb-4">
+        <div class="card p-4 shadow p-3 bg-body-tertiary rounded">
+            <div class="d-flex justify-content-between align-items-end mx-3">
+                <h3 class="fs-4 justify-content-center d-flex mt-3">Alur Pendaftaran</h3>
+                <div class="btn btn-secondary d-flex align-items-center gap-2 py-2 px-3">
+                    <i class="bx bx-edit"></i>
+                    <p>
+                        Ubah
+                    </p>
+                </div>
+            </div>
+            <div class="mt-4 mx-2">
+                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
+                    <p class="p-3">1.</p>
+                    <p class="p-3">Membayar uang pendaftaran</p>
+                </div>
+                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
+                    <p class="p-3">2.</p>
+                    <p class="p-3">Membayar uang pendaftaran</p>
+                </div>
+                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
+                    <p class="p-3">3.</p>
+                    <p class="p-3">Membayar uang pendaftaran</p>
+                </div>
+                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
+                    <p class="p-3">4.</p>
+                    <p class="p-3">Membayar uang pendaftaran</p>
+                </div>
+                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
+                    <p class="p-3">5.</p>
+                    <p class="p-3">Membayar uang pendaftaran</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-6 pe-lg-3">
         <div class="card p-4 shadow p-3 bg-body-tertiary rounded">
             <h3 class="fs-4 justify-content-center d-flex mt-3">Agenda Pendaftaran</h3>
             <div class="teks mt-4 mx-2">
@@ -87,43 +145,6 @@
                     <button type="submit" class="btn btn-success px-3 py-2">Tambah +</button>
                 </div>
             </form>
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-6 pe-lg-3 mb-4">
-        <div class="card p-4 shadow p-3 bg-body-tertiary rounded">
-            <div class="d-flex justify-content-between align-items-end mx-3">
-                <h3 class="fs-4 justify-content-center d-flex mt-3">Alur Pendaftaran</h3>
-                <div class="btn btn-secondary d-flex align-items-center gap-2 py-2 px-3">
-                    <i class="bx bx-edit"></i>
-                    <p>
-                        Ubah
-                    </p>
-                </div>
-            </div>
-            <div class="mt-4 mx-2">
-                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
-                    <p class="p-3">1.</p>
-                    <p class="p-3">Membayar uang pendaftaran</p>
-                </div>
-                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
-                    <p class="p-3">2.</p>
-                    <p class="p-3">Membayar uang pendaftaran</p>
-                </div>
-                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
-                    <p class="p-3">3.</p>
-                    <p class="p-3">Membayar uang pendaftaran</p>
-                </div>
-                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
-                    <p class="p-3">4.</p>
-                    <p class="p-3">Membayar uang pendaftaran</p>
-                </div>
-                <div class="d-flex align-items-center rounded bg-light d-flex mb-2">
-                    <p class="p-3">5.</p>
-                    <p class="p-3">Membayar uang pendaftaran</p>
-                </div>
-            </div>
         </div>
     </div>
     <div class="col-lg-6 ps-lg-3 mb-4">
