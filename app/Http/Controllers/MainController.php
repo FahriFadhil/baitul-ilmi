c95893d120d4ddba510af323f24da3df7c047684
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2023-05-03 14:08:52 +0700

    semi

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Alumni;
+use App\Gallery;
+use App\News;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function home()
+    {
+        $data_berita = News::all();
+        return view('home', compact('data_berita'));
+    }
+    public function profile()
+    {
+        return view('profile');
+    }
+    public function academic()
+    {
+        return view('academic');
+    }
+    public function news()
+    {
+        $data_berita = News::all();
+        return view('news.index', compact('data_berita'));
+    }
+    public function enrollment()
+    {
+        return view('enrollment');
+    }
+    public function gallery()
+    {
+        $data_gallery = Gallery::all();
+        return view('gallery', compact('data_gallery'));
+    }
+    public function alumni()
+    {
+        $data_alumni = Alumni::all();
+        return view('alumni', compact('data_alumni'));
+    }
+    public function dashboard()
+    {
+        return view('dashboard.index');
+    }
+}
--- resources/views/dashboard/enrollment/biaya.blade.php
+++ resources/views/dashboard/enrollment/biaya.blade.php
@@ -1,1 +1,20 @@
 @extends('dashboard.layouts.app')
+
+@section('content')
+<form action="">
+
+  <div class="col-lg-8 ms-auto">
+    <div class="mb-3">
+      <input required name="headline" type="text" class="form-control px-3 py-2" placeholder="Judul Berita">
+    </div>
+    <div class="row gap-4 mb-3">
+      <input required name="author" class="form-control px-3 py-2" type="text" placeholder="Penulis">
+    </div>
+    <div class="mb-3">
+      <textarea name="description" class="form-control px-3 py-2" style="min-height: 16rem;" id="exampleFormControlTextarea1" rows="3" placeholder="Isi Berita"></textarea>
+    </div>
+    <button type="submit" class="btn btn-success px-3 py-2">Buat Berita</button>
+  </div>
+  
+</form>
+@endsection
