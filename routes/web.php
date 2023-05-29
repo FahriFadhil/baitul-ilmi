74e9bb3082418df8a232b6bab68ffea4aea1afb7
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2023-05-29 14:16:26 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@
 
 use App\Http\Controllers\NewsController;
 
-Auth::routes();
+Route::group(['middleware' => 'guest'], function () {
+  Auth::routes();
+});
 
 Route::get('/', 'MainController@home');
 Route::get('/profile', 'MainController@profile');
@@ -22,35 +24,34 @@ Route::get('/news', 'MainController@news');
 Route::get('/news/detail/{id}', 'MainController@news_detail');
 Route::get('/penerimaan', 'MainController@enrollment');
 Route::get('/gallery', 'MainController@gallery');
-Route::get('/alumni', 'MainController@alumni');   
+Route::get('/alumni', 'MainController@alumni');
 
 Route::group(['middleware' => 'auth'], function () {
-  
+
   Route::get('/home', 'MainController@redirect_dashboard');
   Route::get('/dashboard', 'MainController@dashboard');
-  
+
   Route::resource('/dashboard/news', 'NewsController');
   Route::resource('/dashboard/gallery', 'GalleryController');
   Route::resource('/dashboard/alumni', 'AlumniController');
   Route::delete('/dashboard/alumni/destroy/{id}', 'AlumniController@destroy');
-  
+
   Route::post('/dashboard/alumni/create/store', 'GraduationController@store');
   Route::delete('/dashboard/alumni/create/destroy/{id}', 'GraduationController@destroy');
-  
+
   Route::get('/dashboard/pendaftaran', 'MainController@pendaftaran');
-  
+
   Route::resource('/dashboard/enrollment/syarat', 'SyaratPendaftaranController');
   Route::delete('/dashboard/enrollment/syarat/destroy/{id}', 'SyaratPendaftaranController@destroy');
-  
+
   Route::resource('/dashboard/enrollment/agenda', 'AgendaPendaftaranController');
   Route::delete('/dashboard/enrollment/agenda/destroy/{id}', 'AgendaPendaftaranController@destroy');
-  
+
   Route::resource('/dashboard/enrollment/biaya', 'BiayaPendaftaranController');
-  
+
   Route::resource('/dashboard/enrollment/alur', 'AlurPendaftaranController');
-  
+
   Route::resource('/dashboard/enrollment/status', 'StatusPendaftaranController');
-  
 });
 
 
