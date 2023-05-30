fca5449c79673fa45a6d85bf685d72ba3883b636
Fahri Fadhil Muhammad <fahrifadhil.m22@gmail.com>
2023-05-30 10:40:12 +0700

    ok2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(Syarat::class);
         $this->call(Admin::class);
         $this->call(Graduation::class);
+        $this->call(Principal::class);
     }
 }
